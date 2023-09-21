9b5ffc6ad667d0ce727f103c8aa9763a09eb1ffd
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-09-21 10:38:11 +0530

    bug resolved in create-order functionality

--- server/controllers/order.controller.ts
+++ server/controllers/order.controller.ts
@@ -12,67 +12,82 @@ import { newOrder } from "../services/order.service";
 
 export const createOrder = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
-    try{
-        const {courseId, payment_info} = req.req.body as IOrder;
+    try {
+      const { courseId, payment_info } = req.req.body as IOrder;
 
-        const user = await userModel.findById(req.req.user?._id);
+      const user = await userModel.findById(req.req.user?._id);
 
-        const courseExistInUser = user?.courses.some((c:any) => c._id.toString() === courseId);
+      const courseExistInUser = user?.courses.some(
+        (c: any) => c._id.toString() === courseId
+      );
 
-        if(courseExistInUser){
-            return next(new ErrorHandler("You have already purchased this course", 400));
+      if (courseExistInUser) {
+        return next(
+          new ErrorHandler("You have already purchased this course", 400)
+        );
+      }
+
+      const course = await CourseModel.findById(courseId);
+      if (!course) {
+        return next(new ErrorHandler("Course not found", 404));
+      }
+      //   console.log("course", course);
+      const data: any = {
+        courseId: course?._id,
+        userId: user?._id,
+        payment_info,
+      };
+      //   console.log("data",data)
+      const mailData = {
+        order: {
+          _id: course._id.toString().slice(0, 6),
+          name: course.name,
+          price: course.price,
+          date: new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }),
+        },
+      };
+
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/order-confirmation.ejs"),
+        { order: mailData }
+      );
+
+      try {
+        if (user) {
+          await sendMail({
+            email: user.email,
+            subject: "Order Confirmation",
+            template: "order-confirmation.ejs",
+            data: mailData,
+          });
         }
+      } catch (err: any) {
+        return next(new ErrorHandler(err.message, 500));
+      }
 
-        const course = await CourseModel.findById(courseId);
-        if(!course){
-            return next(new ErrorHandler("Course not found", 404));
-        }
-        console.log("course", course);
-        const data:any = {
-            courseId: course?._id,
-            userId: user?._id,
-            payment_info,
-        }
+      user?.courses.push(course?._id);
+      await user?.save();
 
-        
+      await NotificationModel.create({
+        user: user?._id,
+        title: "New Order",
+        message: `You have new order for ${course?.name} course`,
+      });
 
-        const mailData = {
-            order : {
-                _id: course._id.toString().slice(0,6),
-                name: course.name,
-                price: course.price,
-                date: new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'}),
-            }
-        }
+      course.purchased ? (course.purchased += 1) : course.purchased;
 
-        const html = await ejs.renderFile(path.join(__dirname, "../mails/order-confirmation.ejs"), {order: mailData});
-
-        try{
-            if(user){
-                await sendMail({
-                    email: user.email,
-                    subject: "Order Confirmation",
-                    template: "order-confirmation.ejs",
-                    data: mailData,
-                });
-            }
-        }catch(err: any){
-            return next(new ErrorHandler(err.message, 500));
-        }
-
-        user?.courses.push(course?._id);
-        await user?.save();
-
-        await NotificationModel.create({
-            user: user?._id,
-            title: "New Order",
-            message: `You have new order for ${course?.name} course`,
-        });
-
-        newOrder(data, res, next);
-        
-
-    }catch(err: any){
+      await course.save();
+      //   newOrder(data, res, next);
+      const order = await OrderModel.create(data);
+      res.status(200).json({
+        success: true,
+        order,
+      });
+    } catch (err: any) {
       next(new ErrorHandler(err.message, 500));
     }
   }
--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import { createOrder } from '../controllers/order.controller';
+import express from "express";
+import { createOrder } from "../controllers/order.controller";
+import { isAuthenticated } from "../middleware/auth";
 
 const orderRouter = express.Router();
 
-orderRouter.post('/create-order', createOrder);
+orderRouter.post("/create-order", isAuthenticated, createOrder);
 
 export default orderRouter;
