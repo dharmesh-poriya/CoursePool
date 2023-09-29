94f3d0a69324e0c88758fb24a00e75e340b7b22f
jeetsavsani1173 <jeetsavsani1173@gmail.com>
2023-09-29 23:57:04 +0530

    layout router changes : remove auth middleware part from route

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,12 +1,26 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller";
+import {
+  createLayout,
+  editLayout,
+  getLayoutByType,
+} from "../controllers/layout.controller";
 const layoutRouter = express.Router();
 
-layoutRouter.post("/create-layout", isAuthenticated,authorizeRoles("admin"), createLayout);
+layoutRouter.post(
+  "/create-layout",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  createLayout
+);
 
-layoutRouter.put("/edit-layout", isAuthenticated,authorizeRoles("admin"), editLayout);
+layoutRouter.put(
+  "/edit-layout",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editLayout
+);
 
-layoutRouter.get("/get-layout/:type", isAuthenticated,authorizeRoles("admin"), getLayoutByType);
+layoutRouter.get("/get-layout/:type", getLayoutByType);
 
 export default layoutRouter;
