013586cb0f44d498df05592eb556edcc519c4e53
D.Poriya <dharmeshkporiya@gmail.com>
2023-10-01 23:54:39 +0530

    Revert "Revert "server: adding timestamp in courseschema, updating caching process in course controller""
    completing course access page with all functionality

--- client/redux/features/courses/coursesApi.ts
+++ client/redux/features/courses/coursesApi.ts
@@ -34,16 +34,71 @@ export const coursesApi = apiSlice.injectEndpoints({
     }),
     getUsersAllCourses: builder.query({
       query: () => ({
-        url: 'get-courses',
-        method: 'GET',
-        credentials: 'include' as const,
-      })
+        url: "get-courses",
+        method: "GET",
+        credentials: "include" as const,
+      }),
     }),
     getCourseDetails: builder.query({
       query: (id) => ({
         url: `get-course/${id}`,
-        method: 'GET',
-        credentials: 'include' as const,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    getCourseContent: builder.query({
+      query: (id) => ({
+        url: `get-course-content/${id}`,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+    addNewQuestion: builder.mutation({
+      query: ({ question, courseId, contentId }) => ({
+        url: "add-question",
+        body: {
+          question,
+          courseId,
+          contentId,
+        },
+        method: "PUT",
+        credentials: "include" as const,
+      }),
+    }),
+    addAnswerInQuestion: builder.mutation({
+      query: ({ answer, courseId, contentId, questionId }) => ({
+        url: "add-answer",
+        body: {
+          answer,
+          courseId,
+          contentId,
+          questionId,
+        },
+        method: "PUT",
+        credentials: "include" as const,
+      }),
+    }),
+    addReviewInCourse: builder.mutation({
+      query: ({ review, rating, courseId }: any) => ({
+        url: `add-review/${courseId}`,
+        body: {
+          review,
+          rating,
+        },
+        method: "PUT",
+        credentials: "include" as const,
+      }),
+    }),
+    addReplyInReview: builder.mutation({
+      query: ({ comment, courseId, reviewId }: any) => ({
+        url: `add-reply`,
+        body: {
+          comment,
+          courseId,
+          reviewId,
+        },
+        method: "PUT",
+        credentials: "include" as const,
       }),
     }),
   }),
@@ -55,5 +110,10 @@ export const {
   useDeleteCourseMutation,
   useEditCourseMutation,
   useGetUsersAllCoursesQuery,
-  useGetCourseDetailsQuery
+  useGetCourseDetailsQuery,
+  useGetCourseContentQuery,
+  useAddNewQuestionMutation,
+  useAddAnswerInQuestionMutation,
+  useAddReviewInCourseMutation,
+  useAddReplyInReviewMutation,
 } = coursesApi;
