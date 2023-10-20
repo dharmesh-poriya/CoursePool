4cdcb03a25b5f49f28b56774f5683333d2999e99
D.Poriya <dharmeshkporiya@gmail.com>
2023-10-20 18:38:29 +0530

    added line one line for deploment issues

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body
         className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
       >
